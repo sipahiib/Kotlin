7fb877245d73ba72fd43238acc3bb5b2d2ec391f
İbrahim <73345010+sipahiib@users.noreply.github.com>
2021-04-29 22:08:41 +0300

    Add files via upload

---
+++ com/sipahi/erisim/ErisimA.kt
@@ -0,0 +1,16 @@
+package com.sipahi.erisim
+
+//public defaultta
+open class ErisimA { //miras alınabilmesi için open eklendi.
+
+    var a=4 //public
+    public var b=5
+
+    private fun c_sayisi()=4
+    protected fun d_sayisi()
+    {
+        println(5)
+    }
+    internal fun e_sayisi()=6
+}
+
---
+++ com/sipahi/erisim2/ErisimB.kt
@@ -0,0 +1,13 @@
+package com.sipahi.erisim2
+import com.sipahi.erisim.ErisimA
+
+fun main(args: Array<String>) {
+    var erisim=ErisimA()
+
+    println(erisim.a)
+    println(erisim.b)
+//  println(erisim.c_sayisi()) //hata verir
+//  erisim.d_sayisi() //hata verir
+    println(erisim.e_sayisi())
+
+}
